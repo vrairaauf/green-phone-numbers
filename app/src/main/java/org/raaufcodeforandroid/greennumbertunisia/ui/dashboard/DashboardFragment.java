4525e765ed57eaadb190e6a04eea490a9ebeabbf
raauf <vrairaaufabidi@gmail.com>
2023-01-12 10:29:22 +0100

    chgange the design of the dahsboard fragment now i make just a spinner to change the language of the app

--- app/src/main/java/org/raaufcodeforandroid/greennumbertunisia/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/org/raaufcodeforandroid/greennumbertunisia/ui/dashboard/DashboardFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.raaufcodeforandroid.greennumbertunisia.controller.AreaController;
+import org.raaufcodeforandroid.greennumbertunisia.controller.SharedPrefController;
 import org.raaufcodeforandroid.greennumbertunisia.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
-
+    private SharedPrefController sharedPrefController;
+    private AreaController areaController;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
@@ -30,9 +34,49 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sharedPrefController=SharedPrefController.get_instance(getContext());
+        areaController=AreaController.get_instance(getContext(), getActivity());
+        areaController.setLocal(sharedPrefController.get_language());
+
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        int selectedLang;
+        String currentlanguage=sharedPrefController.get_language();
+        if(currentlanguage.equals("en"))
+            selectedLang=0;
+        else if(currentlanguage.equals("fr"))
+            selectedLang=1;
+        else
+            selectedLang=2;
+        binding.languageSpinner.setSelection(selectedLang);
+        binding.languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                String lang;
+                if(position==0){
+                    lang="en";
+                }else if(position==1){
+                    lang="fr";
+                }else{
+                    lang="ar";
+                }
+                sharedPrefController.set_language(lang);
+                areaController.setLocal(sharedPrefController.get_language());
 
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
     }
 
     @Override
